56f42f9a45780cc29a82ef03715045326b13a9b4
Jeba Singh Emmanuel <jebasingh.emmanuel@gmail.com>
2025-02-28 11:10:34 -0800

    pytoml fixes

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ local_path: str = (Path(__file__).parent.parent / "gitingest").as_uri()
 
 setup(
     install_requires=[
-        #f"gitingest @ git+ssh://git@github.com/cyclotruc/gitingest.git@c96a7d3",
-        f"gitingest @ {local_path}",
+        f"gitingest @ git+ssh://git@github.com/cyclotruc/gitingest.git@c96a7d3",
         "click>=8.0.0",
         "toml>=0.10.0",
         "smolagents",
--- src/incident_diagram/diagram.py
+++ src/incident_diagram/diagram.py
@@ -135,11 +135,27 @@ class Diagram:
         )
 
     def _run_with_spinner(self, text:str, function ):
-        spinner = Halo(text=text, spinner='dots')
+        if self._is_notebook():
+            return function()
+        else:
+            spinner = Halo(text=text, spinner='dots')
+            try:
+                result = function()
+                spinner.stop()
+                return result
+            except Exception as e:
+                spinner.fail(f'Failed to run {text}')
+                raise e
+
+    # https://stackoverflow.com/questions/15411967/how-can-i-check-if-code-is-executed-in-the-ipython-notebook
+    def _is_notebook(self) -> bool:
         try:
-            result = function()
-            spinner.stop()
-            return result
-        except Exception as e:
-            spinner.fail(f'Failed to run {text}')
-            raise e
+            shell = get_ipython().__class__.__name__
+            if shell == 'ZMQInteractiveShell':
+                return True   # Jupyter notebook or qtconsole
+            elif shell == 'TerminalInteractiveShell':
+                return False  # Terminal running IPython
+            else:
+                return False  # Other type (?)
+        except NameError:
+            return False      # Probably standard Python interpreter
